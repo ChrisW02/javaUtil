4439dc590d69bee584fef6016a2b472a937fdfde
fa1seW <74900409+fa1seW@users.noreply.github.com>
2022-06-30 08:47:10 +0800

    0630

---
+++ src/TimeDemo.java
@@ -0,0 +1,30 @@
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class TimeDemo {
+    public static void main(String[] args) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println("current date and time: " + currentTime);
+
+        LocalDate date1 = currentTime.toLocalDate();
+        System.out.println("local date: " + date1);
+
+        Month month = currentTime.getMonth();
+        int day = currentTime.getDayOfMonth();
+        int seconds = currentTime.getSecond();
+
+        System.out.println(month + " " + day + " " + seconds);
+
+        LocalDateTime date2 = currentTime.withMonth(7).withDayOfMonth(10).withYear(2012);
+        System.out.println(date2);
+
+        LocalDate date3 = LocalDate.of(2016,Month.DECEMBER,25);
+        System.out.println(date3);
+
+        LocalTime date4 = LocalTime.of(19,00);
+        System.out.println(date4);
+
+        LocalTime date5 = LocalTime.parse("20:15:30");
+        System.out.println(date5);
+    }
+}
