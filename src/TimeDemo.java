0a7e6c8b5049316370432d28d93548494dfac978
fa1seW <74900409+fa1seW@users.noreply.github.com>
2022-06-30 08:51:42 +0800

    0630

--- src/TimeDemo.java
+++ src/TimeDemo.java
@@ -18,14 +18,20 @@ public class TimeDemo {
         LocalDateTime date2 = currentTime.withMonth(7).withDayOfMonth(10).withYear(2012);
         System.out.println(date2);
 
-        LocalDate date3 = LocalDate.of(2016,Month.DECEMBER,25);
+        LocalDate date3 = LocalDate.of(2016, Month.DECEMBER, 25);
         System.out.println(date3);
 
-        LocalTime date4 = LocalTime.of(19,00);
+        LocalTime date4 = LocalTime.of(19, 00);
         System.out.println(date4);
 
         LocalTime date5 = LocalTime.parse("20:15:30");
         System.out.println(date5);
+
+        ZonedDateTime date6 = ZonedDateTime.parse("2016-04-20T19:22:15+01:30[Europe/Paris]");
+        System.out.println(date6);
+
+        ZoneId id = ZoneId.of("Asia/Kolkata");
+        System.out.println("ZoneId: " + id);
 
 
     }
